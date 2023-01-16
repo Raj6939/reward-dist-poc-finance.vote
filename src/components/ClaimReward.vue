12eafd5f9b44593c64ece6d1222b8c44dc595d14
raj6939 <raj.v.patil108@gmail.com>
2023-01-16 14:34:22 +0530

    testing it with wallet-connect

--- src/components/ClaimReward.vue
+++ src/components/ClaimReward.vue
@@ -2,6 +2,19 @@
   <div class="hello mr-5 ml-5">
     <div class="row g-3 align-items-center w-100 mt-4">
       <div class="col-lg-3 col-md-3 text-left">
+        <label for="email" class="col-form-label">Airdrop Id:</label>
+      </div>
+      <div class="col-lg-9 col-md-9 px-0">
+        <input
+          v-model="projectId"
+          type="text"
+          placeholder="Enter airdrop id"          
+          class="form-control w-100"
+        />
+      </div>
+    </div>
+    <div class="row g-3 align-items-center w-100 mt-4">
+      <div class="col-lg-3 col-md-3 text-left">
         <label for="email" class="col-form-label">Wallet Address:</label>
       </div>
       <div class="col-lg-9 col-md-9 px-0">
@@ -17,16 +30,21 @@
     <b-button class="mt-4 ml-4" variant="success" @click="onSubmit"
       >Claim Reward</b-button
     >
+    <b-button class="mt-4 ml-4" variant="danger" @click="disconnectWallet"
+      >Disconnect Wallet</b-button
+    >
     
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Web3 from "web3"
 import  loadweb3  from "../contracts/getWeb3";
 import { abi, address } from "../contracts/merkelDropFactoryAbi";
 import {erc20ABI} from "../contracts/ERC20Contract"
 import {utils} from "web3"
+import { provider } from "../contracts/provider"
 // import {ipfsHashToBytes32} from "../utils/conversionFunctions"
 export default {
   name: "claim-reward",
@@ -35,7 +53,7 @@ export default {
   },
   data() {
     return { 
-        projectId:"63a332743d4f7700123c334e",              
+        projectId:"",              
         newRoot:"",       
         ipfsHash:"",
         walletAddress:"",
@@ -48,7 +66,13 @@ export default {
     goTo() {
       this.$router.push('/')
     },
-    async onSubmit() {  
+    async disconnectWallet() {
+      console.log('dd')
+      await provider.disconnect();      
+    },
+    async onSubmit() {
+      console.log(this.accounts);      
+      try{
       let data =[]    
       const res = await axios.get(`https://bank.influencebackend.xyz/bank/check/${this.walletAddress}`)
       data = [...res.data]
@@ -60,7 +84,10 @@ export default {
       console.log(filteredObject.additionalData.chainId)
       console.log(filteredObject.inputData.hash)
       console.log(filteredObject.version)
-      const web3 = await loadweb3();
+      // const web3 = await loadweb3();
+      await provider.enable();
+      const web3 = new Web3(provider);
+      console.log(web3)
       this.accounts = await web3.eth.getAccounts();
       const contract = new web3.eth.Contract(abi, address);
       const [tree, withdrawn] = await Promise.all([
@@ -80,7 +107,15 @@ export default {
         getProofFromApi
       ).send({from:this.accounts[0]})
       console.log(withDrawToken)
-    },
+    }catch(e) {
+      console.log(e)
+      // location.reload();
+    // await provider.disconnect()
+    //   provider.on("disconnect", (code, reason) => {
+    //   console.log(code, reason);
+    //   });
+      
+    }},
     async getProof(projectId,walletAddress) {
       const proof = await axios.get(`https://bank.influencebackend.xyz/proof/${projectId}/${walletAddress}`)
       return proof?.data?.merkleProof
---
+++ src/contracts/provider.js
@@ -0,0 +1,8 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+export const provider = new WalletConnectProvider({
+  infuraId: 'ab66b962539745f9bc4abd4c5ee2a711',
+});
+
+
+
