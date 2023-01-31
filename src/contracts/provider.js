60e8e3215c18bf29e9273974dda57db550f1ddc1
raj6939 <raj.v.patil108@gmail.com>
2023-01-31 15:19:28 +0530

    commenting mobilewallet for now

--- src/contracts/provider.js
+++ src/contracts/provider.js
@@ -2,35 +2,37 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export const provider = new WalletConnectProvider({
   infuraId: 'ab66b962539745f9bc4abd4c5ee2a711',
-  qrcodeModalOptions: {
-    mobileLinks: [
-    {
-      id: '1',
-      name: 'mainnet',
-      links: {        
-        universal: 'https://wallet-connect-poc.netlify.app/claimreward',
-      },
-    },
-    {
-      id: '5',
-      name: 'goerli',
-      links: {        
-        universal: 'https://wallet-connect-poc.netlify.app/claimreward',
-      },
-    },
-  ],    
-  },
-  walletImages: [
-    {      
-      metaMask: "./assets/metamask.svg",
-    },
-  ],
-  chainImages: [
-    {
-      1: "./assets/ethereum.svg",
-      137: "./assets/polygon.png",
-    },
-  ]
+  // qrcodeModalOptions: {
+  //   mobileLinks: [
+  //   {
+  //     id: 'metamask',
+  //     name: 'metamask',
+  //     links: {  
+  //       native:'https://metamask.app.link/dapp/wallet-connect-poc.netlify.app/claimreward',              
+  //       universal: 'https://wallet-connect-poc.netlify.app/claimreward',
+  //     },
+  //   },
+  //   {
+  //     id: 'rainbow',
+  //     name: 'Rainbow',
+  //     links: {    
+  //       native:'rainbow://',    
+  //       universal: 'https://wallet-connect-poc.netlify.app/claimreward',
+  //     },
+  //   },
+  // ],    
+  // },
+  // walletImages: [
+  //   {      
+  //     metaMask: "./assets/metamask.svg",
+  //   },
+  // ],
+  // chainImages: [
+  //   {
+  //     1: "./assets/ethereum.svg",
+  //     137: "./assets/polygon.png",
+  //   },
+  // ]
 });
 
 
